baef003b2cf1d46e01b96c06786ca3d7264bc0f4
Marcus <Marcussand2@gmail.com>
2023-12-21 15:27:40 +0100

    Removed search button

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,13 +30,10 @@
                                   d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                         </svg>
                     </div>
-                    <input type="search" id="default-search"
+                    <input type="search" id="live-search"
                            class="block w-[35rem] p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
                            placeholder="Search Computer, Laptop..." required>
-                    <button type="submit"
-                            class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">
-                        Search
-                    </button>
+
                 </div>
             </form>
 
